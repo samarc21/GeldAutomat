251d5bcd7b650ea022124b8275ec603b29e4ddda
alexander.peralta@est.ulsa.edu.ni <alexander.peralta@est.ulsa.edu.ni>
2020-08-03 00:30:38 -0600

    Formularios ingresarii y retirar

--- Geldautomat/Retirar.Designer.cs
+++ Geldautomat/Retirar.Designer.cs
@@ -28,13 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_Cordobas = new System.Windows.Forms.Button();
+            this.btn_dolares = new System.Windows.Forms.Button();
+            this.btn_100 = new System.Windows.Forms.Button();
+            this.btn_800 = new System.Windows.Forms.Button();
+            this.btn_600 = new System.Windows.Forms.Button();
+            this.btn_500 = new System.Windows.Forms.Button();
+            this.btn_400 = new System.Windows.Forms.Button();
+            this.btn_200 = new System.Windows.Forms.Button();
+            this.btn_otro = new System.Windows.Forms.Button();
+            this.btn_1000 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_Cordobas
+            // 
+            this.btn_Cordobas.Location = new System.Drawing.Point(59, 145);
+            this.btn_Cordobas.Name = "btn_Cordobas";
+            this.btn_Cordobas.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cordobas.TabIndex = 0;
+            this.btn_Cordobas.Text = "Cordobas";
+            this.btn_Cordobas.UseVisualStyleBackColor = true;
+            this.btn_Cordobas.Click += new System.EventHandler(this.btn_Cordobas_Click);
+            // 
+            // btn_dolares
+            // 
+            this.btn_dolares.Location = new System.Drawing.Point(59, 197);
+            this.btn_dolares.Name = "btn_dolares";
+            this.btn_dolares.Size = new System.Drawing.Size(75, 23);
+            this.btn_dolares.TabIndex = 1;
+            this.btn_dolares.Text = "Dolares";
+            this.btn_dolares.UseVisualStyleBackColor = true;
+            this.btn_dolares.Click += new System.EventHandler(this.btn_dolares_Click);
+            // 
+            // btn_100
+            // 
+            this.btn_100.Location = new System.Drawing.Point(165, 145);
+            this.btn_100.Name = "btn_100";
+            this.btn_100.Size = new System.Drawing.Size(75, 23);
+            this.btn_100.TabIndex = 5;
+            this.btn_100.Text = "100";
+            this.btn_100.UseVisualStyleBackColor = true;
+            this.btn_100.Visible = false;
+            this.btn_100.Click += new System.EventHandler(this.btn_100_Click);
+            // 
+            // btn_800
+            // 
+            this.btn_800.Location = new System.Drawing.Point(281, 238);
+            this.btn_800.Name = "btn_800";
+            this.btn_800.Size = new System.Drawing.Size(75, 23);
+            this.btn_800.TabIndex = 7;
+            this.btn_800.Text = "800";
+            this.btn_800.UseVisualStyleBackColor = true;
+            this.btn_800.Visible = false;
+            this.btn_800.Click += new System.EventHandler(this.btn_800_Click);
+            // 
+            // btn_600
+            // 
+            this.btn_600.Location = new System.Drawing.Point(281, 197);
+            this.btn_600.Name = "btn_600";
+            this.btn_600.Size = new System.Drawing.Size(75, 23);
+            this.btn_600.TabIndex = 8;
+            this.btn_600.Text = "600";
+            this.btn_600.UseVisualStyleBackColor = true;
+            this.btn_600.Visible = false;
+            this.btn_600.Click += new System.EventHandler(this.btn_600_Click);
+            // 
+            // btn_500
+            // 
+            this.btn_500.Location = new System.Drawing.Point(281, 145);
+            this.btn_500.Name = "btn_500";
+            this.btn_500.Size = new System.Drawing.Size(75, 23);
+            this.btn_500.TabIndex = 9;
+            this.btn_500.Text = "500";
+            this.btn_500.UseVisualStyleBackColor = true;
+            this.btn_500.Visible = false;
+            this.btn_500.Click += new System.EventHandler(this.btn_500_Click);
+            // 
+            // btn_400
+            // 
+            this.btn_400.Location = new System.Drawing.Point(165, 238);
+            this.btn_400.Name = "btn_400";
+            this.btn_400.Size = new System.Drawing.Size(75, 23);
+            this.btn_400.TabIndex = 10;
+            this.btn_400.Text = "400";
+            this.btn_400.UseVisualStyleBackColor = true;
+            this.btn_400.Visible = false;
+            this.btn_400.Click += new System.EventHandler(this.btn_400_Click);
+            // 
+            // btn_200
+            // 
+            this.btn_200.Location = new System.Drawing.Point(165, 197);
+            this.btn_200.Name = "btn_200";
+            this.btn_200.Size = new System.Drawing.Size(75, 23);
+            this.btn_200.TabIndex = 11;
+            this.btn_200.Text = "200";
+            this.btn_200.UseVisualStyleBackColor = true;
+            this.btn_200.Visible = false;
+            this.btn_200.Click += new System.EventHandler(this.btn_200_Click);
+            // 
+            // btn_otro
+            // 
+            this.btn_otro.Location = new System.Drawing.Point(398, 197);
+            this.btn_otro.Name = "btn_otro";
+            this.btn_otro.Size = new System.Drawing.Size(115, 23);
+            this.btn_otro.TabIndex = 12;
+            this.btn_otro.Text = "Otra cantidad";
+            this.btn_otro.UseVisualStyleBackColor = true;
+            this.btn_otro.Visible = false;
+            this.btn_otro.Click += new System.EventHandler(this.btn_otro_Click);
+            // 
+            // btn_1000
+            // 
+            this.btn_1000.Location = new System.Drawing.Point(398, 145);
+            this.btn_1000.Name = "btn_1000";
+            this.btn_1000.Size = new System.Drawing.Size(75, 23);
+            this.btn_1000.TabIndex = 13;
+            this.btn_1000.Text = "1000";
+            this.btn_1000.UseVisualStyleBackColor = true;
+            this.btn_1000.Visible = false;
+            this.btn_1000.Click += new System.EventHandler(this.btn_1000_Click);
             // 
             // Retirar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_1000);
+            this.Controls.Add(this.btn_otro);
+            this.Controls.Add(this.btn_200);
+            this.Controls.Add(this.btn_400);
+            this.Controls.Add(this.btn_500);
+            this.Controls.Add(this.btn_600);
+            this.Controls.Add(this.btn_800);
+            this.Controls.Add(this.btn_100);
+            this.Controls.Add(this.btn_dolares);
+            this.Controls.Add(this.btn_Cordobas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Retirar";
             this.Text = "Retirar";
@@ -43,5 +171,16 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Cordobas;
+        private System.Windows.Forms.Button btn_dolares;
+        private System.Windows.Forms.Button btn_100;
+        private System.Windows.Forms.Button btn_800;
+        private System.Windows.Forms.Button btn_600;
+        private System.Windows.Forms.Button btn_500;
+        private System.Windows.Forms.Button btn_400;
+        private System.Windows.Forms.Button btn_200;
+        private System.Windows.Forms.Button btn_otro;
+        private System.Windows.Forms.Button btn_1000;
     }
 }
--- Geldautomat/Retirar.cs
+++ Geldautomat/Retirar.cs
@@ -16,5 +16,66 @@ namespace Geldautomat
         {
             InitializeComponent();
         }
+
+        private void btn_Cordobas_Click(object sender, EventArgs e)
+        {
+            seleccion_moneda();
+        }
+        private void btn_dolares_Click(object sender, EventArgs e)
+        {
+            seleccion_moneda();
+        }
+        public void seleccion_moneda()
+        {
+            btn_100.Visible = true;
+            btn_200.Visible = true;
+            btn_400.Visible = true;
+            btn_500.Visible = true;
+            btn_600.Visible = true;
+            btn_800.Visible = true;
+            btn_1000.Visible = true;
+            btn_otro.Visible = true;
+        }
+
+
+        private void btn_100_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_200_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_400_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_500_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_600_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_800_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_1000_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_otro_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
