4ad909691d822af06f94788ac95774f71ec3ab55
lendy.ocampo@est.ulsa.edu.ni <lendy.ocampo@est.ulsa.edu.ni>
2020-08-04 06:08:17 -0600

    Cambie interfaz de Form1 e ingresar pd: aun me falta

--- Geldautomat/Form1.Designer.cs
+++ Geldautomat/Form1.Designer.cs
@@ -31,18 +31,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.pnl_bienvenido = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btn_x = new System.Windows.Forms.Button();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.pnl_menu = new System.Windows.Forms.Panel();
             this.btn_ingresar = new MetroFramework.Controls.MetroButton();
             this.btn_registrarse = new MetroFramework.Controls.MetroButton();
             this.pnl_input = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.pnl_bienvenido.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
             this.pnl_menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -61,8 +64,8 @@
             // 
             // pnl_bienvenido
             // 
-            this.pnl_bienvenido.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.pnl_bienvenido.Controls.Add(this.pictureBox1);
+            this.pnl_bienvenido.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.pnl_bienvenido.Controls.Add(this.label1);
             this.pnl_bienvenido.Controls.Add(this.btn_x);
             this.pnl_bienvenido.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnl_bienvenido.Location = new System.Drawing.Point(4, 4);
@@ -71,22 +74,12 @@
             this.pnl_bienvenido.Size = new System.Drawing.Size(1234, 78);
             this.pnl_bienvenido.TabIndex = 0;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(417, -21);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(424, 116);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            // 
             // btn_x
             // 
             this.btn_x.BackColor = System.Drawing.Color.Red;
             this.btn_x.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_x.ForeColor = System.Drawing.Color.White;
-            this.btn_x.Location = new System.Drawing.Point(1190, -4);
+            this.btn_x.Location = new System.Drawing.Point(1186, 0);
             this.btn_x.Margin = new System.Windows.Forms.Padding(4);
             this.btn_x.Name = "btn_x";
             this.btn_x.Size = new System.Drawing.Size(48, 26);
@@ -98,8 +91,8 @@
             // tableLayoutPanel2
             // 
             this.tableLayoutPanel2.ColumnCount = 2;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.53945F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.46055F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.41707F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 78.58293F));
             this.tableLayoutPanel2.Controls.Add(this.pnl_menu, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.pnl_input, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -113,50 +106,86 @@
             // 
             // pnl_menu
             // 
-            this.pnl_menu.BackColor = System.Drawing.Color.White;
+            this.pnl_menu.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.pnl_menu.Controls.Add(this.pictureBox2);
+            this.pnl_menu.Controls.Add(this.pictureBox1);
             this.pnl_menu.Controls.Add(this.btn_ingresar);
             this.pnl_menu.Controls.Add(this.btn_registrarse);
             this.pnl_menu.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnl_menu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnl_menu.ForeColor = System.Drawing.Color.White;
             this.pnl_menu.Location = new System.Drawing.Point(4, 4);
             this.pnl_menu.Margin = new System.Windows.Forms.Padding(4);
             this.pnl_menu.Name = "pnl_menu";
-            this.pnl_menu.Size = new System.Drawing.Size(184, 505);
+            this.pnl_menu.Size = new System.Drawing.Size(257, 505);
             this.pnl_menu.TabIndex = 0;
             // 
             // btn_ingresar
             // 
             this.btn_ingresar.Highlight = true;
-            this.btn_ingresar.Location = new System.Drawing.Point(34, 178);
+            this.btn_ingresar.Location = new System.Drawing.Point(69, 156);
             this.btn_ingresar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_ingresar.Name = "btn_ingresar";
             this.btn_ingresar.Size = new System.Drawing.Size(126, 35);
-            this.btn_ingresar.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.btn_ingresar.Style = MetroFramework.MetroColorStyle.Silver;
             this.btn_ingresar.TabIndex = 2;
             this.btn_ingresar.Text = "Iniciar sesión";
-            this.btn_ingresar.Theme = MetroFramework.MetroThemeStyle.Dark;
             this.btn_ingresar.Click += new System.EventHandler(this.btn_ingresar_Click_1);
             // 
             // btn_registrarse
             // 
             this.btn_registrarse.Highlight = true;
-            this.btn_registrarse.Location = new System.Drawing.Point(34, 253);
+            this.btn_registrarse.Location = new System.Drawing.Point(69, 371);
             this.btn_registrarse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_registrarse.Name = "btn_registrarse";
             this.btn_registrarse.Size = new System.Drawing.Size(126, 35);
-            this.btn_registrarse.Style = MetroFramework.MetroColorStyle.Orange;
+            this.btn_registrarse.Style = MetroFramework.MetroColorStyle.Silver;
             this.btn_registrarse.TabIndex = 3;
             this.btn_registrarse.Text = "Registrar";
+            this.btn_registrarse.Theme = MetroFramework.MetroThemeStyle.Light;
             this.btn_registrarse.Click += new System.EventHandler(this.btn_registrarse_Click_1);
             // 
             // pnl_input
             // 
+            this.pnl_input.BackColor = System.Drawing.Color.MediumAquamarine;
             this.pnl_input.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnl_input.Location = new System.Drawing.Point(196, 4);
+            this.pnl_input.Location = new System.Drawing.Point(269, 4);
             this.pnl_input.Margin = new System.Windows.Forms.Padding(4);
             this.pnl_input.Name = "pnl_input";
-            this.pnl_input.Size = new System.Drawing.Size(1042, 505);
+            this.pnl_input.Size = new System.Drawing.Size(969, 505);
             this.pnl_input.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.label1.Font = new System.Drawing.Font("Sketch 3D", 52.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(330, -4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(597, 93);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bienvenidos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(88, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(88, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(88, 283);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(88, 83);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
             // 
             // Dashboard
             // 
@@ -171,9 +200,11 @@
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.pnl_bienvenido.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnl_bienvenido.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.pnl_menu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -188,6 +219,8 @@
         private System.Windows.Forms.Panel pnl_input;
         private MetroFramework.Controls.MetroButton btn_ingresar;
         private MetroFramework.Controls.MetroButton btn_registrarse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Geldautomat/IngresarII.Designer.cs
+++ Geldautomat/IngresarII.Designer.cs
@@ -28,61 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IngresarII));
             this.txt_user = new System.Windows.Forms.TextBox();
             this.txt_loginpin = new System.Windows.Forms.TextBox();
             this.Usuario = new System.Windows.Forms.Label();
             this.Pin = new System.Windows.Forms.Label();
             this.btn_ingresarLog = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_user
             // 
-            this.txt_user.Location = new System.Drawing.Point(251, 160);
+            this.txt_user.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.txt_user.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_user.Location = new System.Drawing.Point(178, 164);
+            this.txt_user.Multiline = true;
             this.txt_user.Name = "txt_user";
-            this.txt_user.Size = new System.Drawing.Size(100, 22);
+            this.txt_user.Size = new System.Drawing.Size(148, 27);
             this.txt_user.TabIndex = 15;
             // 
             // txt_loginpin
             // 
-            this.txt_loginpin.Location = new System.Drawing.Point(251, 194);
+            this.txt_loginpin.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.txt_loginpin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_loginpin.Location = new System.Drawing.Point(178, 227);
+            this.txt_loginpin.Multiline = true;
             this.txt_loginpin.Name = "txt_loginpin";
-            this.txt_loginpin.Size = new System.Drawing.Size(100, 22);
+            this.txt_loginpin.Size = new System.Drawing.Size(148, 27);
             this.txt_loginpin.TabIndex = 16;
             // 
             // Usuario
             // 
             this.Usuario.AutoSize = true;
-            this.Usuario.Location = new System.Drawing.Point(143, 163);
+            this.Usuario.Font = new System.Drawing.Font("Baskerville Old Face", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Usuario.Location = new System.Drawing.Point(62, 164);
             this.Usuario.Name = "Usuario";
-            this.Usuario.Size = new System.Drawing.Size(57, 17);
+            this.Usuario.Size = new System.Drawing.Size(93, 27);
             this.Usuario.TabIndex = 17;
             this.Usuario.Text = "Usuario";
             // 
             // Pin
             // 
             this.Pin.AutoSize = true;
-            this.Pin.Location = new System.Drawing.Point(143, 199);
+            this.Pin.Font = new System.Drawing.Font("Baskerville Old Face", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pin.Location = new System.Drawing.Point(108, 227);
             this.Pin.Name = "Pin";
-            this.Pin.Size = new System.Drawing.Size(28, 17);
+            this.Pin.Size = new System.Drawing.Size(47, 27);
             this.Pin.TabIndex = 18;
             this.Pin.Text = "Pin";
             // 
             // btn_ingresarLog
             // 
-            this.btn_ingresarLog.Location = new System.Drawing.Point(225, 264);
+            this.btn_ingresarLog.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ingresarLog.Location = new System.Drawing.Point(127, 324);
             this.btn_ingresarLog.Name = "btn_ingresarLog";
-            this.btn_ingresarLog.Size = new System.Drawing.Size(103, 35);
+            this.btn_ingresarLog.Size = new System.Drawing.Size(131, 39);
             this.btn_ingresarLog.TabIndex = 19;
             this.btn_ingresarLog.Text = "Ingresar";
             this.btn_ingresarLog.UseVisualStyleBackColor = true;
             this.btn_ingresarLog.Click += new System.EventHandler(this.btn_ingresarLog_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(21, 154);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 37);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(67, 221);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 33);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 21;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(264, 324);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(44, 44);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 22;
+            this.pictureBox3.TabStop = false;
+            // 
             // IngresarII
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(380, 528);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btn_ingresarLog);
             this.Controls.Add(this.Pin);
             this.Controls.Add(this.Usuario);
@@ -93,6 +142,9 @@
             this.Name = "IngresarII";
             this.Text = "IngresarII";
             this.Load += new System.EventHandler(this.IngresarII_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,5 +156,8 @@
         private System.Windows.Forms.Label Usuario;
         private System.Windows.Forms.Label Pin;
         private System.Windows.Forms.Button btn_ingresarLog;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
