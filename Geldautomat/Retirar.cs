dde6f1a9902c351f4843ae5238c53aa1ce08115c
Melissa Garcia <melissagarcia9920@gmail.com>
2020-08-03 18:35:59 -0600

    billetes

--- Geldautomat/Retirar.cs
+++ Geldautomat/Retirar.cs
@@ -13,6 +13,12 @@ namespace Geldautomat
     public partial class Retirar : Form
     {
         int cantidad = 0;
+        int moneda = 0;
+        int a = 10, b = 10, c = 13, d = 24;     //cordobas
+        int e = 100, f = 100, g = 80, h = 100, i = 60;
+        int cont = 0;
+       
+
         public Retirar()
         {
             InitializeComponent();
@@ -93,8 +99,85 @@ namespace Geldautomat
         }*/
 
        public void comparar()
+                            //contador de billetes
         {
+            int ca=0, cb=0, cc=0, cd=0, ce=0, cf=0, cg=0,ch=0, ci= 0;
+            switch (moneda)
+            {
+         
+                case 1:                 //cordobas
+                    if (cantidad >= 1000)
+                    {
+                        cont = cont - 1000;
+                        cantidad = cantidad - 1000;
+                        a = a - 1;
+                        ca = +1;
+                    }
+                    if (cantidad >= 500)
+                    {
+                        cont = cont - 500;
+                        cantidad = cantidad - 500;
+                        b = b - 1;
+                        cb = +1;
+                    }
+                    if (cantidad >= 200)
+                    {
+                        cont = cont - 200;
+                        cantidad = cantidad - 200;
+                        c = c - 1;
+                        cc = +1;
+                    }
+                    if (cantidad >= 100)
+                    {
+                        cont = cont - 100;
+                        cantidad = cantidad - 100;
+                        d = d - 1;
+                        cd = +1;
+                    }
+                    break;
 
+
+                case 2:                             //dolares
+                    if (cantidad >= 100)
+                    {
+                        cont = cont - 100;
+                        cantidad = cantidad - 100;
+                        e = e - 1;
+                        ce = +1;
+                    }
+                    if (cantidad >= 50)
+                    {
+                        cont = cont - 50;
+                        cantidad = cantidad - 50;
+                        f = f - 1;
+                        cf = +1;
+                    }
+                    if (cantidad >= 30)
+                    {
+                        cont = cont - 30;
+                        cantidad = cantidad - 30;
+                        g = g - 1;
+                        cg = +1;
+                    }
+                    if (cantidad >= 20)
+                    {
+                        cont = cont - 20;
+                        cantidad = cantidad - 20;
+                        h = h - 1;
+                        ch = +1;
+                    }
+                    if (cantidad >= 10)
+                    {
+                        cont = cont - 10;
+                        cantidad = cantidad - 10;
+                        i = i - 1;
+                        ci = +1;
+                    }
+
+
+                    break;
+
+            }
         }
 
         /*private void btn_retirar_Click_1(object sender, EventArgs e)
